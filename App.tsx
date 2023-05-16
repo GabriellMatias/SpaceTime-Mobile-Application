2f6c24574e604bf51248cab6534ed55560afdcb1
GabriellMatias <gabriel.goiasat@gmail.com>
2023-05-15 21:52:39 -0300

    :rocket: Feat: Setting basics configs

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='bg-gray-700 flex-1'>
+      <Text className='text-white m-auto font-bold'>Hellow word</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+
